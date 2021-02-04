797758f10dc58cb0b499d1e47d52e10b20180a4a
cwpent <77705144+cwpent@users.noreply.github.com>
2021-02-04 16:07:26 -0500

    Database fix attempt

--- Migrations/20210204205658_Initial.Designer.cs
+++ Migrations/20210204205658_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YellowBucket.Data;
 
 namespace YellowBucket.Migrations
 {
     [DbContext(typeof(YellowBucketContext))]
-    partial class YellowBucketContextModelSnapshot : ModelSnapshot
+    [Migration("20210204205658_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210204205658_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YellowBucket.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_RatingId",
+                table: "Movie",
+                column: "RatingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Rating_RatingId",
+                table: "Movie",
+                column: "RatingId",
+                principalTable: "Rating",
+                principalColumn: "RatingId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Rating_RatingId",
+                table: "Movie");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movie_RatingId",
+                table: "Movie");
+        }
+    }
+}
