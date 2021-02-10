35ea5f042f4fd02c26e5dba32dc989dd762937dd
ngiongbdnsiog <nidsofvnsdifng@gmail.com>
2021-02-09 21:55:31 -0500

    Update Genre.cs
    YellowBucket

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-//F
+//YellowBucket
 namespace YellowBucket.Models
 {
     public class Genre
